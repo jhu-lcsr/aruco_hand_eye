78c98258b2ead8cd888361f8b073cdf9b34cfc90
Jonathan Bohren <jbo@jhu.edu>
2014-11-23 00:40:01 -0500

    cleaning up, working independently, adding readme

---
+++ README.md
@@ -0,0 +1,80 @@
+
+ARUCO / VISP Hand-Eye Calibration
+=================================
+
+## Use Cases
+
+This package uses the ARUCO planar target tracker from `aruco_ros` and the VISP
+hand-eye calibration from `visp_hand2eye_calibration` to provide a simple
+camera pose estimation package.
+
+If you're unfamiliar with Tsai's hand-eye calibration [1], it can be used in two ways:
+
+- **eye-in-hand** -- To compute the static transform from a robot's
+  end-effector to the optical frame of a camera. In this case, the camera is
+  mounted on the end-effector, and you place the visual target so that it is
+  fixed relative to the base of the rboot.
+- **eye-on-base** -- To compute the static transform from a robot's base to the
+  optical frame of a camera. In this case, the camera is mounted to the base of
+  the robot (or kinematic chain), and you place the visual target so that it is
+  fixed relative to the end-effector of the robot.
+
+## Usage
+
+For both use cases, you can either launch the `aruco_hand_eye.launch`
+launchfile, or you can include it in another launchfile as shown below. Either
+way, the launchfile will bring up the `aruco_ros` tracker and the
+`visp_hand2eye_calibration` solver, along with an integration script. By
+default, the integration script will interactively ask you to accept or discard
+each sample.
+
+### eye-in-hand
+
+```xml
+<launch>
+  <include file="$(find aruco_hand_eye)/launch/aruco_hand_eye.launch">
+    <arg name="markerid"   value="582"/>
+    <arg name="markersize" value="0.141"/>
+    <arg name="publish_tf" value="true"/>
+
+    <arg name="marker_parent_frame" value="/base_link"/>
+    <arg name="camera_parent_frame" value="/ee_link"/>
+
+    <arg name="camera" value="/camera/rgb"/>
+    <arg name="camera_frame" value="/camera_rgb_optical_frame"/>
+  </include>
+</launch>
+```
+
+### eye-on-base
+
+```xml
+<launch>
+  <include file="$(find aruco_hand_eye)/launch/aruco_hand_eye.launch">
+    <arg name="markerid"   value="582"/>
+    <arg name="markersize" value="0.141"/>
+    <arg name="publish_tf" value="true"/>
+
+    <arg name="marker_parent_frame" value="/ee_link"/>
+    <arg name="camera_parent_frame" value="/base_link"/>
+
+    <arg name="camera" value="/camera/rgb"/>
+    <arg name="camera_frame" value="/camera_rgb_optical_frame"/>
+  </include>
+</launch>
+```
+
+## Examples
+
+For calibrating a kinect mounted to the base of a manipulator that can grasp the target:
+
+```
+roslaunch aruco_hand_eye kinect.launch ee_frame:=/my/robot/ee_link
+```
+
+## References
+
+[1] *Tsai, Roger Y., and Reimar K. Lenz. "A new technique for fully autonomous
+and efficient 3D robotics hand/eye calibration." Robotics and Automation, IEEE
+Transactions on 5.3 (1989): 345-358.*
+
--- src/aruco_hand_eye/hand_eye_connector.py
+++ src/aruco_hand_eye/hand_eye_connector.py
@@ -5,16 +5,32 @@ import tf_conversions.posemath as tfconv
 import PyKDL
 
 from geometry_msgs.msg import Vector3, Quaternion, Transform, TransformStamped
-from visp_hand2eye_calibration.msg import TransformArray 
-from visp_hand2eye_calibration.srv import compute_effector_camera_quick 
+from visp_hand2eye_calibration.msg import TransformArray
+from visp_hand2eye_calibration.srv import compute_effector_camera_quick
 
 class HandEyeConnector(object):
     def __init__(self):
 
-        self.camera_marker_frame = rospy.get_param('~camera_marker_frame')
-        self.hand_marker_frame = rospy.get_param('~hand_marker_frame')
+        # Frame which is rigidly attached to the camera
+        # The transform from this frame to the camera optical frame is what
+        # we're trying to compute.
+        # For the eye-in-hand case, this is the end-effector frame.
+        # For the eye-on-base case, this is the world or base frame.
+        self.camera_parent_frame_id = rospy.get_param('~camera_parent_frame')
+
+        # Frame which is rigidly attached to the marker
+        # The transform from the camera parent frame to the marker parent frame
+        # is given by forward kinematics.
+        # For the eye-in-hand case, this is the world or base frame.
+        # For the eye-on-base case, this is the end-effector frame.
+        self.marker_parent_frame_id = rospy.get_param('~marker_parent_frame')
+
         self.publish_tf = rospy.get_param('~publish_tf')
         self.tf_suffix = rospy.get_param('~tf_suffix')
+        self.sample_rate = rospy.get_param('~sample_rate')
+        self.interactive = rospy.get_param('~interactive')
+
+        # Compute the camera base to optical transform
         self.xyz_optical_base = rospy.get_param('~xyz_optical_base', [0,0,0])
         self.rpy_optical_base = rospy.get_param('~rpy_optical_base', [0,0,0])
         self.F_optical_base = PyKDL.Frame(
@@ -22,67 +38,50 @@ class HandEyeConnector(object):
                 PyKDL.Vector(*self.xyz_optical_base))
         self.F_base_optical = self.F_optical_base.Inverse()
 
+        # tf structures
         self.listener = tf.TransformListener()
         self.broadcaster = tf.TransformBroadcaster()
 
-        self.aruco_subscriber = rospy.Subscriber(
-                'aruco_tracker/transform', 
-                TransformStamped, 
-                self.aruco_cb, 
-                queue_size=1)
+        # rate limiter
+        self.rate = rospy.Rate(self.sample_rate)
 
-        rospy.wait_for_service('compute_effector_camera_quick')
-        self.calibrate = rospy.ServiceProxy(
-                'compute_effector_camera_quick', 
-                compute_effector_camera_quick)
-
-        self.rate = rospy.Rate(2)
+        # input data
         self.hand_world_samples = TransformArray()
         self.camera_marker_samples = TransformArray()
 
+        # marker subscriber
+        self.aruco_subscriber = rospy.Subscriber(
+                'aruco_tracker/transform',
+                TransformStamped,
+                self.aruco_cb,
+                queue_size=1)
 
-    def aruco_cb(self, msg):
+        # calibration service
+        rospy.wait_for_service('compute_effector_camera_quick')
+        self.calibrate = rospy.ServiceProxy(
+                'compute_effector_camera_quick',
+                compute_effector_camera_quick)
 
-        rospy.loginfo("Received marker sample.")
-
-        parent_frame_id = msg.header.frame_id
-
-        try:
-            self.listener.waitForTransform(self.hand_marker_frame, '/world', msg.header.stamp, rospy.Duration(0.1))
-            (trans,rot) = self.listener.lookupTransform(self.hand_marker_frame, '/world', msg.header.stamp)
-        except tf.Exception as ex:
-            rospy.logwarn(str(ex))
-            return
-        
-        self.hand_world_samples.header.frame_id = parent_frame_id
-        self.hand_world_samples.transforms.append(Transform(Vector3(*trans), Quaternion(*rot)))
-
-        self.camera_marker_samples.header.frame_id = parent_frame_id
-        self.camera_marker_samples.transforms.append(msg.transform)
-
-        if len(self.hand_world_samples.transforms) != len(self.camera_marker_samples.transforms):
-            rospy.logerr("Different numbers of hand-world and camera-marker samples.")
-            return
-
-        n_min = 2
-        if len(self.hand_world_samples.transforms) < n_min:
-            rospy.logerr("%d more samples needed..." % (n_min-len(self.hand_world_samples.transforms)))
-            return
-
+    def compute_calibration(self, msg):
         rospy.loginfo("Computing from %g poses..." % len(self.hand_world_samples.transforms) )
+        result = None
+
+        # Get the camera optical frame for convenience
+        optical_frame_id = msg.header.frame_id
+
         try:
             result = self.calibrate(self.camera_marker_samples, self.hand_world_samples)
         except rospy.ServiceException as ex:
             rospy.logerr("Calibration failed: "+str(ex))
-            return
+            return None
 
         if self.publish_tf:
             self.broadcaster.sendTransform(
                     (result.effector_camera.translation.x, result.effector_camera.translation.y, result.effector_camera.translation.z),
                     (result.effector_camera.rotation.x, result.effector_camera.rotation.y, result.effector_camera.rotation.z, result.effector_camera.rotation.w),
                     rospy.Time.now(),
-                    parent_frame_id + self.tf_suffix,
-                    'world')
+                    optical_frame_id + self.tf_suffix,
+                    self.camera_parent_frame_id)
 
         rospy.loginfo("Result:\n"+str(result))
 
@@ -101,6 +100,53 @@ class HandEyeConnector(object):
 
         rospy.loginfo("Base xyz: ( %f %f %f ) rpy: ( %f %f %f ) xyzw: ( %f %f %f %f )" % (xyz+rpy+xyzw))
 
-        self.rate.sleep()
-    
+        return result
 
+    def aruco_cb(self, msg):
+
+        rospy.loginfo("Received marker sample.")
+
+        # Get the camera optical frame for convenience
+        optical_frame_id = msg.header.frame_id
+
+        try:
+            # Get the transform between the marker and camera frames (from FK)
+            self.listener.waitForTransform(
+                self.marker_parent_frame_id, self.camera_parent_frame_id,
+                msg.header.stamp, rospy.Duration(0.1))
+
+            (trans,rot) = self.listener.lookupTransform(
+                self.marker_parent_frame_id, self.camera_parent_frame_id,
+                msg.header.stamp)
+        except tf.Exception as ex:
+            rospy.logwarn(str(ex))
+            return
+
+        # Update data
+        self.hand_world_samples.header.frame_id = optical_frame_id
+        self.hand_world_samples.transforms.append(Transform(Vector3(*trans), Quaternion(*rot)))
+
+        self.camera_marker_samples.header.frame_id = optical_frame_id
+        self.camera_marker_samples.transforms.append(msg.transform)
+
+        if len(self.hand_world_samples.transforms) != len(self.camera_marker_samples.transforms):
+            rospyend-effector.logerr("Different numbers of hand-world and camera-marker samples.")
+            return
+
+        n_min = 2
+        if len(self.hand_world_samples.transforms) < n_min:
+            rospy.logwarn("%d more samples needed..." % (n_min-len(self.hand_world_samples.transforms)))
+            return
+
+        if not self.compute_calibration(msg):
+            return
+
+        # interactive
+        if self.interactive:
+            i = raw_input('Hit [enter] to accept this latest sample, or `d` to discard: ')
+            if i == 'd':
+                del self.hand_world_samples[-1]
+                del self.camera_marker_samples[-1]
+                self.compute_calibration(msg)
+        else:
+            self.rate.sleep()
